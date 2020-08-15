f96401ccfaf4a88ef29d6596f91bbac096d6ff16
ahlev <ohahlev@gmail.com>
2020-08-15 12:29:05 +0700

    update

--- index.js
+++ index.js
@@ -2,5 +2,6 @@ const EngineAddon = require('ember-engines/lib/engine-addon');
 'use strict';
 
 module.exports = EngineAddon.extend({
-  name: 'ember-engine-ahlev-hero'
+  name: 'ember-engine-ahlev-hero',
+  lazyLoading: false
 });
